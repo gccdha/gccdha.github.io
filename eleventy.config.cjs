d37ca3832d8be53c05fed9148f0a3bf411ac5a34
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-12-16 21:01:19 -0700

    added profile and background

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,4 +1,14 @@
-module.exports = async function(eleventyConfig) {};
+const markdownIt = require("markdown-it");
+const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+
+
+module.exports = async function(eleventyConfig) {
+  let options = {
+    html:true
+  };
+  eleventyConfig.setLibrary("md", markdownIt(options));
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+};
 
 module.exports.config = {
   dir: {
